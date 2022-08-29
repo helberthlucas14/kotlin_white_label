f064d524c37f815cf8ea28426cb64b761f3fd2c9
Helberth Versiani <helberthlucas@ioasys.com.br>
2022-08-29 13:41:15 -0300

    initial configuration

---
+++ app/src/main/java/com/example/kotlin_white_label/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.kotlin_white_label.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.kotlin_white_label.R
+import com.example.kotlin_white_label.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+}
---
+++ app/src/main/java/com/example/kotlin_white_label/ui/products/ProductsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin_white_label.ui.products
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kotlin_white_label.databinding.FragmentProductsBinding
+
+class ProductsFragment : Fragment() {
+
+    private var _binding: FragmentProductsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
